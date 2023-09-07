0b5f9e0cc8e8f66ebb286619e48c0a3684ab6770
Jingsheng Chang <jcha0222@student.monash.edu>
2023-09-07 11:38:22 +1000

    finish user story 2.2, upload new logo

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
+# import libraries
 import os
 import mysql.connector
 from flask_sqlalchemy import SQLAlchemy
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for, jsonify,
+from flask import (Flask,
+                   render_template, 
+                   request,
+                   send_from_directory,
+                   jsonify,
                    Response)
 
+# Create Flask app
 app = Flask(__name__)
 
 # Set password
 PASSWORD = "seamTA07"
 
+# Set up basic authentication
 @app.before_request
 def require_basic_auth():
     auth = request.authorization
@@ -35,6 +41,7 @@ db = SQLAlchemy(app)
 class MelbourneHousingData(db.Model):
     __tablename__ = "melbourne_housing_data"
     my_row_id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String)
     suburb = db.Column(db.String)
     rooms = db.Column(db.Integer)
     bathroom = db.Column(db.Integer)
@@ -43,17 +50,18 @@ class MelbourneHousingData(db.Model):
     longitude = db.Column(db.Double)
     car = db.Column(db.Integer)
 
-
+# index page
 @app.route('/', methods=['GET'])
 def index():
    print('Request for index page received')
    return render_template('index.html')
 
+# favicon
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico')
 
+# browsing & query properties page
 @app.route('/browsing', methods=['GET'])
 def browsing():
    print('Request for browsing page received')
@@ -72,6 +80,9 @@ def browsing():
    
    # Get min price of properties from database
    min_price = db.session.query(db.func.min(MelbourneHousingData.price)).scalar()
+
+   # Get max price of properties from database
+   max_price = db.session.query(db.func.max(MelbourneHousingData.price)).scalar()
    
    # Get latitude and longitude of properties from the database
    coordinates = db.session.query(MelbourneHousingData.latitude, MelbourneHousingData.longitude).all()
@@ -83,6 +94,7 @@ def browsing():
                            min_price=min_price, 
                            coordinates=coordinates)
 
+# query properties
 @app.route('/browsing_post', methods=['POST'])
 def browsing_post():
    # accept the request data from the client
@@ -102,7 +114,8 @@ def browsing_post():
    properties_info = [(property.latitude, property.longitude, property.rooms, property.bathroom, property.car, property.price) for property in properties]
 
    return jsonify(properties_info)
-   
+
+# suburb statistics  
 @app.route('/avg', methods=['POST'])
 def avg():
    data = request.json
@@ -112,8 +125,24 @@ def avg():
    avg_price = db.session.query(db.func.avg(MelbourneHousingData.price)).filter(MelbourneHousingData.suburb == suburb).scalar()
    avg_car = db.session.query(db.func.avg(MelbourneHousingData.car)).filter(MelbourneHousingData.suburb == suburb).scalar()
    return jsonify(
-      [round(avg_bedrooms), round(avg_bathrooms), round(avg_car), round(avg_price, 2)]
+      [round(avg_bedrooms), round(avg_bathrooms), round(avg_car), round(avg_price)]
       )
+
+# top3 properties
+@app.route('/top3', methods=['POST'])
+def top3():
+    data = request.json
+    suburb = data['suburb']
+    bedrooms = data['bedrooms']
+    bathrooms = data['bathrooms']
+    maxPrice = data['maxPrice']
+    properties = db.session.query(MelbourneHousingData).filter(MelbourneHousingData.suburb == suburb,
+                                                               MelbourneHousingData.rooms == bedrooms, 
+                                                               MelbourneHousingData.bathroom == bathrooms,
+                                                                MelbourneHousingData.price <= maxPrice
+                                                               ).order_by(MelbourneHousingData.price).limit(3).all()
+    properties_info = [(property.address, property.price) for property in properties]
+    return jsonify(properties_info)
 
 @app.route('/prediction', methods=['GET'])
 def prediction():
